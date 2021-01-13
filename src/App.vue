365486fae9a3b7304bdf2fc6156d4b7587bb68ee
Алексей Орлов <orlov@proitr.ru>
2021-01-13 22:39:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div id="app">
+    <section class="content">
+      <chat-header/>
+      <div class="flex-row">
+        <div class="sidebar">
+          <chat-list/>
+        </div>
+        <div class="main">
+          <chat/>
+        </div>
+      </div>
+    </section>
+    <div class="mobile-app-notice">
+      <div class="mobile-app-notice__text">
+        Простите но для мобильных телефонов у нас есть мобильное приложение
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatHeader from './components/ChatHeader'
+import ChatList from './components/ChatList'
+import Chat from './components/Chat'
+
+export default {
+  name: 'App',
+  components: {
+    ChatHeader,
+    ChatList,
+    Chat,
+  },
+}
+</script>
+
+<style lang="sass">
+html, body
+  overflow: auto
+  height: 100%
+  font-size: 14px
+  font-family: 'Exo 2', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #333333
+  margin: 0
+  padding: 0
+  width: 100%
+
+#app
+  text-align: left
+  width: 100%
+  height: 100%
+  position: relative
+
+.mobile-app-notice
+  display: none
+
+  @media (max-width: 699px)
+    display: flex
+    font-size: 36px
+    color: #999999
+    text-align: center
+    align-items: center
+    justify-content: center
+    height: 100%
+
+    &__text
+      margin-top: -20%
+      padding: 0 10%
+
+.chat-icon
+  width: 40px
+  height: 40px
+  fill: #ffffff
+
+.content
+  display: flex
+  height: 100%
+  flex-direction: column
+
+  @media (max-width: 699px)
+    display: none
+
+.sidebar
+  width: 20.8%
+  border-right: 1px solid #333333
+
+.main
+  width: 79.2%
+
+.flex-row
+  display: flex
+</style>
---
+++ src/components/Chat.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="chat">
+    <div class="chat__header">
+      <div class="date">{{ date }}</div>
+    </div>
+    <div class="chat__body">
+      <div class="message" :class="{'message__my': item.isMy}" v-for="(item, index) in messages" :key="index">
+        <div v-if="!item.isMy" class="message__user">{{ item.name }}</div>
+        <div class="message__text" :class="{'message__text--blue': item.isMy}">
+          {{ item.text }}
+          <div class="message__date" :class="{'message__date--blue': item.isMy}">{{ item.time }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="chat__footer">
+      <chat-textarea/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatTextarea from './ChatTextarea'
+
+export default {
+  name: 'Chat',
+  components: {
+    ChatTextarea,
+  },
+  data()
+  {
+    return {
+      date: '13 января',
+      messages: [
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Привет, как твои дела? У меня все хорошо и хорошее настроение. Решил тебе написать и' +
+              'узнать какое настроение у тебя. Надеюсь оно у тебя очень хорошее. Ведь сегодня такой' +
+              'прекрасный день и так тепло.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Привет! Да все прекрасно и хорошо.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Чем планируешь сегодня заниматься?',
+          time: '10:45',
+        },
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Я планирую сегодня съездить отдохнуть куда нибудь на озеро. Провести время с семьёй и' +
+              'близкими. Советую и тебе в такую погоду тоже развеяться и отдохнуть. Скоро лето' +
+              'закончится и уже будет холодно.',
+          time: '10:45',
+        },
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Привет, как твои дела? У меня все хорошо и хорошее настроение. Решил тебе написать и' +
+              'узнать какое настроение у тебя. Надеюсь оно у тебя очень хорошее. Ведь сегодня такой' +
+              'прекрасный день и так тепло.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Привет! Да все прекрасно и хорошо.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Чем планируешь сегодня заниматься?',
+          time: '10:45',
+        },
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Я планирую сегодня съездить отдохнуть куда нибудь на озеро. Провести время с семьёй и' +
+              'близкими. Советую и тебе в такую погоду тоже развеяться и отдохнуть. Скоро лето' +
+              'закончится и уже будет холодно.',
+          time: '10:45',
+        }, {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Привет, как твои дела? У меня все хорошо и хорошее настроение. Решил тебе написать и' +
+              'узнать какое настроение у тебя. Надеюсь оно у тебя очень хорошее. Ведь сегодня такой' +
+              'прекрасный день и так тепло.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Привет! Да все прекрасно и хорошо.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Чем планируешь сегодня заниматься?',
+          time: '10:45',
+        },
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Я планирую сегодня съездить отдохнуть куда нибудь на озеро. Провести время с семьёй и' +
+              'близкими. Советую и тебе в такую погоду тоже развеяться и отдохнуть. Скоро лето' +
+              'закончится и уже будет холодно.',
+          time: '10:45',
+        },
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Привет, как твои дела? У меня все хорошо и хорошее настроение. Решил тебе написать и' +
+              'узнать какое настроение у тебя. Надеюсь оно у тебя очень хорошее. Ведь сегодня такой' +
+              'прекрасный день и так тепло.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Привет! Да все прекрасно и хорошо.',
+          time: '10:45',
+        },
+        {
+          isMy: true,
+          name: 'Я',
+          text: 'Чем планируешь сегодня заниматься?',
+          time: '10:45',
+        },
+        {
+          isMy: false,
+          name: 'Сафронов А.',
+          text: 'Я планирую сегодня съездить отдохнуть куда нибудь на озеро. Провести время с семьёй и' +
+              'близкими. Советую и тебе в такую погоду тоже развеяться и отдохнуть. Скоро лето' +
+              'закончится и уже будет холодно.',
+          time: '10:45',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped lang="sass">
+.chat
+  height: 100%
+  display: flex
+  flex-direction: column
+  justify-content: flex-end
+  position: relative
+
+.chat__header
+  padding: 33px 0 15px
+  height: 42px
+  text-align: center
+  position: absolute
+  top: 0
+  width: 100%
+  background-color: #fff
+  z-index: 1
+
+  .date
+    font-size: 16px
+    color: #ffffff
+    background-color: #333333
+    display: inline-block
+    padding: 9px 49px 11px
+    font-weight: 900
+    border-radius: 5px
+
+.chat__body
+  display: flex
+  flex-direction: column
+  padding-bottom: 20px
+  overflow-y: scroll
+  height: calc(100vh - 190px)
+
+  .scroll
+    z-index: -1
+
+.message
+  max-width: 47%
+  padding: 5px 27px
+
+  &__user
+    margin-left: 23px
+    color: #999999
+    margin-bottom: 8px
+
+  &__my
+    align-self: flex-end
+
+  &__text
+    padding: 14px 20px
+    background-color: #ebebeb
+    border-radius: 5px
+    position: relative
+
+    &--blue
+      background-color: #cae5ff
+      padding-right: 57px
+
+  &__date
+    position: absolute
+    bottom: 6px
+    right: 18px
+    color: #999999
+
+    &--blue
+      color: #66ccff
+
+.chat__footer
+  border-top: 1px solid #333333
+  height: 92px
+
+</style>
